720f74a58622031fdebf7c6b24f701f3f306c308
nadeemsaleem3 <nadeemsaleem.bwp@gmail.com>
2025-01-09 09:50:34 +0500

    init

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-
+require('dotenv').config();
 require("./conn/conn");
 
 const auth = require("./routes/auth");
--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+// Use environment variables to store your DB credentials
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+
 const conn = async (req,res) => {
    try {
-    await mongoose.connect("mongodb+srv://nadeemsaleemforwork:xVSHzasoOeYfxNKB@cluster0.9vshb.mongodb.net/").then(()=> {
+    await mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@cluster0.mongodb.net`).then(()=> {
         console.log("MongoDB Connected");
     });
    } catch (error) {
